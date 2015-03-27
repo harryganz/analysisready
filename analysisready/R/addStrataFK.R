f9963da5d86b5bc99fb83daa0dc11241b5709a85
Harry Ganz <talktotheganz@gmail.com>
2015-03-27 10:58:38 -0400

    Completed first version of addStrataFK
    Haven't tested it yet

--- analysisready/R/addStrataFK.R
+++ analysisready/R/addStrataFK.R
@@ -6,38 +6,66 @@
 #' which a record belongs
 addStrataFK  <- function(x){
   ## Add AVG_PSU_DEPTH, to be dropped later
-  
-  
+  d  <- addAvgPsuDepth(x);
   ## Drop records before april in 2004 and 2005
-  f1  <- function(d){
-    return(subset(d, !(YEAR>=2004 & YEAR<=2005 & MONTH<4)));
-  }
+  d  <- subset(d, YEAR >= 2004 & YEAR <= 2005 & MONTH < 4);
   
-  ## Logic for formation of STRATA
-  f2  <- function(d){
-    d$STRAT  <- ifelse(
+  ## Assigning Strata divided into 3 branches ##
+  
+  ## branch 1
+  branch1  <- function(){
+    ifelse(
       HABITAT_CD == "SPGR_HR",
       "HRRF",
       ifelse(
         ZONE_NR < 4,
-        ifelse(
-          YEAR == 1994 || YEAR == 1996,
-          "MCPR",
-          ifelse(
-            ZONE_NR == 2,
-            "MCPR",
-            ifelse(
-              ZONE_NR == 1,
-              "INPR",
-              "OFPR"
-              )
-            )
-          ),
+        branch2(),
         ifelse(
           AVG_PSU_DEPTH < 18,
-          
+          branch3(),
+          ifelse(
+            PROT == 1,
+            "FMLR",
+            ifelse(
+              YEAR < 2001,
+              "FMLR",
+              "FDLR"
+              )
+            )
           )
         )
       )
+  }
+  ## Branch 2: if ZONE_NR < 4
+  branch2  <- function(){
+    ifelse(
+      YEAR == 1994 || YEAR == 1996 || ZONE_NR == 2,
+      "MCPR",
+      ifelse(
+        ZONE_NR == 1,
+        "INPR",
+        "OFPR"
+        )
+      )
+  }
+  ## Branch 3: if ZONE_NR == 4 & AVG_PSU_DEPTH >= 18m
+  branch3  <- function(){
+    ifelse(
+      AVG_PSU_DEPTH >= 6,
+      "FMLR",
+      ifelse(
+        YEAR == 1994 || YEAR == 1996 || 
+          (YEAR == 1998 & PROT == 1),
+        "FMLR",
+        "FSLR"
+        )
+      )
+    ## Assign STRAT column
+    d$STRAT  <- with(d, branch1());
+    ## Drop AVG_PSU_DEPTH
+    drop  <- names(d) == "AVG_PSU_DEPTH"
+    out  <- d[-drop];
+    
+    return(out)
   }
 }
