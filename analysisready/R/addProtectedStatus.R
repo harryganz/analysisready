99810c645f50f537c24b11797bb02677ed7a3aa7
Harry Ganz <hganz@rsmas.miami.edu>
2015-05-01 12:30:31 -0400

    Changed protected status for YEAR <= 2000 in DRTO
    All protected areas in DRTO before 2000 are given a protected status of 2

--- analysisready/R/addProtectedStatus.R
+++ analysisready/R/addProtectedStatus.R
@@ -13,7 +13,7 @@ addProtectedStatus  <- function(x){
   out  <- x;
   out$PROT  <- with(out,
                     ifelse(
-                      SUBREGION_NR == 4,
+                      SUBREGION_NR == 4 | (SUBREGION_NR == 3 & YEAR <= 2000),
                       2,
                       ifelse(
                         MPA_NR > 0,
