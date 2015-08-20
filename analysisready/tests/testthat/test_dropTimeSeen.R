355f6077795a237c2930b74902fe4e38820bc910
Harry Ganz <hganz@rsmas.miami.edu>
2015-08-20 14:10:04 -0400

    Fix test
    Changed fixture and updated test

--- analysisready/tests/testthat/test_dropTimeSeen.R
+++ analysisready/tests/testthat/test_dropTimeSeen.R
@@ -1,16 +1,14 @@
 context("testing dropTimeSeen")
 lookup  <- data.frame(
-  SPECIES_CD = c("EPI MORI","MYC BONA"),
-  KEEP_10MIN = c(0, 1))
+  SPECIES_CD = c("LUT GRIS","EPI MORI","MYC BONA"),
+  KEEP_10MIN = c(0,0, 1))
 data  <- data.frame(
   SPECIES_CD = c("EPI MORI", "EPI MORI","MYC BONA", "MYC BONA",
                  "LUT GRIS", "LUT GRIS"),
-  TIME_SEEN = c(3, 2, 3, 2, 3, 2)
-  )
-
-expect_equal(dropTimeSeen(data, lookup)$TIME_SEEN,
-             c(2,3,2,2))
-expect_equal(dropTimeSeen(data)$TIME_SEEN,
-             c(2,2,2))
-expect_true(dropTimeSeen(data, lookup)$SPECIES_CD[2] == "MYC BONA")
+  TIME_SEEN = c(3, 2, 3, 2, 3, 2),
+  NUM = rep(1, 6)
+  );
+expect_equal(dropTimeSeen(data, lookup)$NUM,
+             c(0,1,1,1,0,1))
+expect_true(dropTimeSeen(data, lookup)$SPECIES_CD[3] == "MYC BONA")
 expect_error(dropTimeSeen())
